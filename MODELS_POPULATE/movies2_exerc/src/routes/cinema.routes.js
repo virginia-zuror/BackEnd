f3933f90c446b9b6adca03070ba929660f70da47
virginia-zuror <virginiazuor@gmail.com>
2023-03-02 20:38:28 +0100

    feat put-movie $ pushing

--- MODELS_POPULATE/movies2_exerc/src/routes/cinema.routes.js
+++ MODELS_POPULATE/movies2_exerc/src/routes/cinema.routes.js
@@ -22,28 +22,41 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.put("/:id", async(req, res, next)=>{
+/* router.put("/:id", async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const cinemaModified = new Cinema(req.body);
-    cinemaModified._id= id;
+    cinemaModified._id = id;
     const cinemaUpdated = await Cinema.findByIdAndUpdate(id, cinemaModified);
     return res.status(200).json(cinemaUpdated);
   } catch (error) {
-    return next(error)
+    return next(error);
   }
-  });
-  
-  router.delete("/:id", async(req, res, next)=>{
-    try {
-      const {id} = req.params;
-      await Cinema.findByIdAndDelete(id);
-      return res.status(200).json("cinema deleted!");
-    } catch (error) {
-      return next(error)
-    }
-  })
+}); */
 
+router.put("/add-movie", async(req, res, next) => {
+  try {
+    const { movieId } = req.body;
+    const { cinemaId } = req.body;
+    const updatedCinema = await Cinema.findByIdAndUpdate(
+      cinemaId,
+      { $push: { movies: movieId } },
+      { new: true }
+    );
+    return res.status(200).json(updatedCinema);
+  } catch (error) {
+    return next(error);
+  }
+});
 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Cinema.findByIdAndDelete(id);
+    return res.status(200).json("cinema deleted!");
+  } catch (error) {
+    return next(error);
+  }
+});
 
 module.exports = router;
